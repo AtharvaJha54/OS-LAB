087d05cfb8279de3c55ee2876615c56f6d2cd6ec
AtharvaJha54 <atharvajha.cs24@bmsce.ac.in>
2026-02-23 12:33:05 +0530

    Add files via upload

---
+++ WEEK 1/practice4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int visited[n];
+
+    for(i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(visited[i] == 1)
+            continue;
+
+        int duplicateCount = 1;
+
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                visited[j] = 1;
+                duplicateCount++;
+            }
+        }
+
+        if(duplicateCount > 1) {
+            count++;
+        }
+    }
+
+    printf("Total number of duplicate elements = %d\n", count);
+
+    return 0;
+}
